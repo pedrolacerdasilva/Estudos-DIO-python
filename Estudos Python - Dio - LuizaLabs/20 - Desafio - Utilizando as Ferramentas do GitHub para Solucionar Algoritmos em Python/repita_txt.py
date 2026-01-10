ab9ae434f308610e28ddf1013b93ef4ecff580c8
pedrolacerdasilva <pedro.lacerda.silva.pl@gmail.com>
2026-01-10 19:13:24 -0300

    Add files via upload
    Desafios github colilot

---
+++ Estudos Python - Dio - LuizaLabs/20 - Desafio - Utilizando as Ferramentas do GitHub para Solucionar Algoritmos em Python/repita_txt.py
@@ -0,0 +1,9 @@
+#Solicitar uma string e um numero inteiro como entrada
+#retornar a string repetida o numero de vezes indicado pelo usuario
+
+string_entrada = input("Digite uma frase: ")
+numero_repeticoes = int(input("digite o numero de vezes que deseja repetir a frase: "))
+
+resultado = string_entrada * numero_repeticoes
+print("Resultado da repetição: ", resultado)
+
